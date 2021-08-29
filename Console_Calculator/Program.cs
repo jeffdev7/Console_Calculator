a3009ab3addff33a93ff263460a2ff33cb0578c5
jeffdev <jefferson-67@live.com>
2021-08-29 16:58:12 -0300

    Add project files.

---
+++ Console_Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Console_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("\nCalculator");
+                Console.WriteLine("");
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("1- Addition");
+                Console.WriteLine("2- Subtraction");
+                Console.WriteLine("3- Multiplication");
+                Console.WriteLine("4- Division");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("0- Exit");
+                Console.WriteLine("-------------------------");
+
+                Console.Write("Option:");
+                string option = Console.ReadLine();
+                double a = 0;
+                double b = 0;
+
+                if (option == "0") Environment.Exit(0);
+
+                Console.Write("A=");
+                a = double.Parse(Console.ReadLine());
+                Console.Write("B=");
+                b = double.Parse(Console.ReadLine());
+
+                if (option == "1")
+                    Console.WriteLine(a + " + " + b + " = " + (a + b).ToString());
+                else if (option == "2")
+                    Console.WriteLine(a + " - " + b + " = " + (a - b).ToString());
+                else if (option == "3")
+                    Console.WriteLine(a + " x " + b + " = " + (a * b).ToString());
+                else if (option == "4")
+                    Console.WriteLine(a + " : " + b + " = " + (a / b).ToString("0.00"));
+            }
+        }
+    }
+}
